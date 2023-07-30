df7c0924a7d3d1deab690816b358e7a01eab0c0a
Joshua Reed <11220408+jreed1701@users.noreply.github.com>
2023-07-30 14:47:57 -0600

    Remove hard coded path.

--- application/gui/frontend.py
+++ application/gui/frontend.py
@@ -1,34 +1,40 @@
-import os 
+import os
 
-from PyQt5.QtWidgets import QMainWindow, QHBoxLayout, QAction, QLayout, QWidget, QSystemTrayIcon, QMenu, QApplication
+from PyQt5.QtWidgets import QAction, QSystemTrayIcon, QMenu, QApplication
 from PyQt5.QtGui import QIcon
 
+from application.gui.globals import GuiGlobals
 
-class GuiApp():
+
+class GuiApp:
+    def __init__(self) -> None:
+        self._globals = GuiGlobals()
 
     def initialize(self):
-        
         app = QApplication([])
         app.setQuitOnLastWindowClosed(False)
-        
+
         # Adding an icon
-        icon = QIcon(r"C:\projects\pgsm_agent\application\gui\resources\keeper.png")
-        
+        current_file = os.path.abspath(__file__)
+        current_folder = os.path.dirname(current_file)
+        icon_path = os.path.join(current_folder, "resources", "keeper.png")
+        icon = QIcon(icon_path)
+
         # Adding item on the menu bar
         tray = QSystemTrayIcon()
         tray.setIcon(icon)
         tray.setVisible(True)
-        
+
         # Creating the options
         menu = QMenu()
         option1 = QAction("Games")
         menu.addAction(option1)
-        
+
         # To quit the app
         quit = QAction("Quit")
         quit.triggered.connect(app.quit)
         menu.addAction(quit)
-        
+
         # Adding options to the System Tray
         tray.setContextMenu(menu)
 
--- application/gui/globals.py
+++ application/gui/globals.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 
-class GuiGlobals:
-    
-    def __init__(self):
 
+class GuiGlobals:
+    def __init__(self):
         self._DESCRIPTION_MSG = """ """
-        
-        self._EPILOG_MSG = '''
+
+        self._EPILOG_MSG = """
             Examples:
-        '''
-        
+        """
+
         self._VERSION = "_alpha"
